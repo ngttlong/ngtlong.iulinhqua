b324e68b3ce9bfd86f9a0c8e0427a29c7c9901cf
Nguyễn Tài Long <164600107+ngttlong@users.noreply.github.com>
2025-06-09 22:12:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Anh iu bé ,Anh hứa hongg làm bé buồn nữa đouuu :3";
+  titleElement.innerHTML = "Long iu Linh lam , Tui hongg làm Linh buồn nữa đouuu :3";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
@@ -43,11 +43,11 @@ function resizeYesButton() {
 function generateMessage(noCount) {
   const messages = [
     "Không Bao Giờ",
-    "Anh bicc lỗi rồi ạa",
-    "Mong bé tha lỗi choo anhh :((",
-    "Anhh saii rồi , anhh đáng trách ạ",
-    "Bé đừng giận anhh nữa nhoo",
-    "Anhhh iu bé nhắm nhunnn đóoooo",
+    "Tui bicc lỗi rồi ạa",
+    "Mong Linh tha lỗi choo tui :((",
+    "Tui saii rồi , tui đáng trách ạ",
+    "Linh đừng giận tui nữa maaaa",
+    "Tui iu Linh nhắm nhunnn đóoooo",
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
